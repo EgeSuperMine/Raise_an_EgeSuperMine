e3067b9016fe839e78d9f573179bac4899fd3569
EgeSuperMine <103334869+EgeSuperMine@users.noreply.github.com>
2023-12-24 21:30:40 +0300

    Update and rename RaE_Savecs to RaE_Save.cs

---
+++ RaE_Save.cs
@@ -0,0 +1,10 @@
+// Raise an EgeSuperMine Save Modules.
+
+// Raise an EgeSuperMine BETA 1.0
+decimal Game.Money;
+int Game.Health;
+int Game.Hunger;
+int Game.AntiDeaths;
+string Game.GameDifficulty;
+bool Shop.Bought_ESMSpray;
+bool Shop.Bought_AutoFeeder;
