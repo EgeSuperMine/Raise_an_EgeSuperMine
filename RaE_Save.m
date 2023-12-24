545f96db335ce8dfe164dedb22da640d644064d7
EgeSuperMine <103334869+EgeSuperMine@users.noreply.github.com>
2023-12-24 21:38:20 +0300

    Update and rename RaE_Save.cs to RaE_Save.m

---
+++ RaE_Save.m
@@ -0,0 +1,51 @@
+Raise an EgeSuperMine Save Modules.
+
+Raise an EgeSuperMine BETA 1.0
+v v v v v v v v v v v v v v v
+decimal Game.Money;
+int Game.Health;
+int Game.Hunger;
+int Game.AntiDeaths;
+string Game.GameDifficulty;
+bool Shop.Bought_ESMSpray;
+bool Shop.Bought_AutoFeeder;
+bool Shop.Bought_InvisibleHat;
+bool Shop.Bought_Autoclicker;
+bool Shop.Bought_Poster;
+bool Shop.Bought_Floor2Key;
+bool Shop.Bought_LavaLamp;
+bool Shop.Bought_Sword;
+bool Shop.Bought_NewPC;
+bool Shop.Bought_TV;
+bool Shop.Bought_DoorC4;
+int Shop.Bought_Bottle;
+bool Shop.Bought_GF;
+bool Shop.Bought_AutoSave;
+DateTime Game.Save1_CreatedTime;
+DateTime Game.Save1_UpdatedTime;
+bool Game.mods_fastesm;
+
+Raise an EgeSuperMine BETA 1.1
+v v v v v v v v v v v v v v v
+decimal Game.Money;
+int Game.Health;
+int Game.Hunger;
+int Game.AntiDeaths;
+string Game.GameDifficulty;
+bool Shop.Bought_ESMSpray;
+bool Shop.Bought_AutoFeeder;
+bool Shop.Bought_InvisibleHat;
+bool Shop.Bought_Autoclicker;
+bool Shop.Bought_Poster;
+bool Shop.Bought_Floor2Key;
+bool Shop.Bought_LavaLamp;
+bool Shop.Bought_Sword;
+bool Shop.Bought_NewPC;
+bool Shop.Bought_TV;
+bool Shop.Bought_DoorC4;
+int Shop.Bought_Bottle;
+bool Shop.Bought_GF;
+bool Shop.Bought_AutoSave;
+DateTime Game.Save1_CreatedTime;
+DateTime Game.Save1_UpdatedTime;
+bool Game.mods_fastesm;
